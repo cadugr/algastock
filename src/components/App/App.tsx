d58470ae9f637d4a1851e798fde6d0f820d369e9
cadugr <cadu.guerra@gmail.com>
2020-09-18 22:58:14 -0300

    Removendo o ClassComponent do App e utilizando o hook useEffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TestComponent from '../TestComponent'
-import ClassComponent from '../ClassComponent'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <TestComponent name="Cadu"/>
-        <ClassComponent name="mundo"/>
         <a
           className="App-link"
           href="https://reactjs.org"
