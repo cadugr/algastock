2af413bab22f397ed07c8fa0d70087de00b4d4e3
cadugr <cadu.guerra@gmail.com>
2020-09-19 11:33:16 -0300

    Limpando toda a aplicação

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="Cadu"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
