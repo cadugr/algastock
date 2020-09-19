cf47aa13ed880228041e69386c2e593a9d1ae4c7
cadugr <cadu.guerra@gmail.com>
2020-09-19 15:38:13 -0300

    adicionando o componente de botão

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../Button';
+
+function TestComponent () {
+  return <img width="16" src="https://image.flaticon.com/icons/png/512/1617/1617460.png" alt="search icon"/>
+}
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock"/>
-      App
+      <div className="Container">
+        <Button
+          onClick={() => window.alert('UIIIU')}
+          appendIcon={<TestComponent/>}
+        >
+          Alert
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Button.css'
+
+declare interface ButtonProps {
+    content?: string
+    onClick?: () => void
+    appendIcon: JSX.Element
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+    return <button className="AppButton"
+            onClick={props.onClick}
+        >
+        {props.children || 'Nameless Button'}
+        {props.appendIcon}
+    </button>
+}
+
+export default Button
