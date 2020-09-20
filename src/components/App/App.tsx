b80645d6ace259d7d60576cdb591dec6cd691f2f
cadugr <cadu.guerra@gmail.com>
2020-09-20 16:33:58 -0300

    Limpando o componente App e preparando componente Table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input/Input';
 
-function TestComponent () {
-  return <img width="16" src="https://image.flaticon.com/icons/png/512/1617/1617460.png" alt="search icon"/>
-}
 
 function App() {
-  const [street, setStreet] = useState('')
+  
   return (
     <div className="App">
       <Header title="AlgaStock"/>
       <Container>
-        <ul>
-          {
-            ['Daniel', 'Willian', 'Thiago', 'Daniel'].map((name,index) => {
-              return <li key={index}>
-                {name}
-              </li>
-            })
-          }
-        </ul>
+        
       </Container>
     </div>
   );
