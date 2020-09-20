2c186f36834d4273b1ef5aa911781c8a8599ad62
cadugr <cadu.guerra@gmail.com>
2020-09-20 15:48:37 -0300

    adicionando component Input

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input/Input';
 
 function TestComponent () {
   return <img width="16" src="https://image.flaticon.com/icons/png/512/1617/1617460.png" alt="search icon"/>
 }
 
 function App() {
+  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title="AlgaStock"/>
@@ -19,6 +21,11 @@ function App() {
         >
           Alert
         </Button>
+        <Input label="Street"
+               placeholder="E.g.: 15th Avenue"
+               value={street}
+               onChange={e => setStreet(e.target.value)}
+        />
       </Container>
     </div>
   );
