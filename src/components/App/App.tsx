30076f7f7d76a55774cbe6c11773953baa1c6fda
cadugr <cadu.guerra@gmail.com>
2020-09-20 15:22:30 -0300

    adicionando o component container e movendo o componente e botão para a pasta shared

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../Button';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
 
 function TestComponent () {
   return <img width="16" src="https://image.flaticon.com/icons/png/512/1617/1617460.png" alt="search icon"/>
@@ -11,14 +12,14 @@ function App() {
   return (
     <div className="App">
       <Header title="AlgaStock"/>
-      <div className="Container">
+      <Container>
         <Button
           onClick={() => window.alert('UIIIU')}
           appendIcon={<TestComponent/>}
         >
           Alert
         </Button>
-      </div>
+      </Container>
     </div>
   );
 }
